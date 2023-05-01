1483ec68bca238453ef254685832cad59e5b263b
eugene663 <yujin663@gmail.com>
2023-05-01 16:11:44 +0900

    record and make a file

---
+++ src/pages/Record/Record.js
@@ -0,0 +1,88 @@
+import React, { useState, useCallback } from "react";
+
+const AudioRecord = () => {
+  const [stream, setStream] = useState();
+  const [media, setMedia] = useState();
+  const [onRec, setOnRec] = useState(true);
+  const [source, setSource] = useState();
+  const [analyser, setAnalyser] = useState();
+  const [audioUrl, setAudioUrl] = useState();
+
+  const onRecAudio = () => {
+    // 음원정보를 담은 노드를 생성, 음원 실행, 디코딩
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    const analyser = audioCtx.createScriptProcessor(0, 1, 1);
+    setAnalyser(analyser);
+
+    function makeSound(stream) {
+      const source = audioCtx.createMediaStreamSource(stream);
+      setSource(source);
+      source.connect(analyser);
+      analyser.connect(audioCtx.destination);
+    }
+    // 마이크 사용 권한 획득
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      const mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      setStream(stream);
+      setMedia(mediaRecorder);
+      makeSound(stream);
+
+      analyser.onaudioprocess = function (e) {
+        // 10분(6000초) 지나면 녹음 중지
+        if (e.playbackTime > 6000) {
+          stream.getAudioTracks().forEach(function (track) {
+            track.stop();
+          });
+          mediaRecorder.stop();
+          // 메서드 호출 노드 연결 해제
+          analyser.disconnect();
+          audioCtx.createMediaStreamSource(stream).disconnect();
+
+          mediaRecorder.ondataavailable = function (e) {
+            setAudioUrl(e.data);
+            setOnRec(true);
+          };
+        } else {
+          setOnRec(false);
+        }
+      };
+    });
+  };
+
+  // 음성 녹음 중지
+  const offRecAudio = () => {
+    media.ondataavailable = function (e) {
+      setAudioUrl(e.data);
+      setOnRec(true);
+    };
+
+    // 오디오 스트림 정지
+    stream.getAudioTracks().forEach(function (track) {
+      track.stop();
+    });
+
+    // 미디어 캡처 중지
+    media.stop();
+    analyser.disconnect();
+    source.disconnect();
+  };
+
+  const onSubmitAudioFile = useCallback(() => {
+    if (audioUrl) {
+      console.log(URL.createObjectURL(audioUrl)); // 출력된 링크 -> 녹음된 오디오
+    }
+    // File 생성자를 통해 파일로 변환
+    const sound = new File([audioUrl], "soundBlob", { lastModified: new Date().getTime(), type: "audio" });
+    console.log(sound); // File 정보 출력
+  }, [audioUrl]);
+
+  return (
+    <>
+      <button onClick={onRec ? onRecAudio : offRecAudio}>녹음</button>
+      <button onClick={onSubmitAudioFile}>결과 확인</button>
+    </>
+  );
+};
+
+export default AudioRecord;
