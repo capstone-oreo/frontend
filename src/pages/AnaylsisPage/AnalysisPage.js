331d0ffafe4136e1b5c45d3fbae22e0bbe056f27
eugene663 <yujin663@gmail.com>
2023-05-21 17:13:52 +0900

    feat: 목소리 분석 그래프 구현

--- src/pages/AnaylsisPage/AnalysisPage.js
+++ src/pages/AnaylsisPage/AnalysisPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Graph from "./Graph"
 import "../../css/AnalysisPage.css"
 
 export default function Analysis() {
@@ -16,7 +17,7 @@ export default function Analysis() {
   useEffect(() => {
     axios.get("/api/records/test", {
       params: {
-        fileId: "fileId"
+        fileId: "129387askdhiuh3"
       }
     })
     .then(function (response) {
@@ -29,19 +30,46 @@ export default function Analysis() {
       });
     }, []);
 
+
   return(
     <>
       <div className="speech">Speech</div>  
       <div className="maru">Maru</div>
       <p className="voice-analysis">목소리 분석</p>
-      <p>Speed: {data.speed[0]}</p>
-          <p>Volume: {data.volume[0]}</p>
-          <p>Keyword: {data.keyword[0]}</p>
-          <p>Habitual World: {data.habitualWorld[0]}</p>
-          <p>Created At: {data.createdAt}</p>
+      <p>{data.id}</p>
+      <p>volume: {data.volume[0]}</p>
+      <div>
+      {data.volume.map((speedValue, index) => (
+        <p key={index}>{index} {speedValue}</p>
+      ))}
+    </div>
       <p className="speed">발표 속도</p>
-
+      <div className="speed-graph">
+        <Graph data={[
+        {
+          id: "speed",
+          data: data.speed.map((value, index) => ({
+            x: index,
+            y: value,
+          })),
+        },
+      ]} color={'#FFF855'} id="speed"/> 
+      </div>
+      <div className="speed-details"></div>
       <p className="volume">목소리 크기</p>
+      <div className="volume-graph">
+        <Graph data={[
+          {
+            id: "volume",
+            data: data.volume.map((value, index) => ({
+              x: index,
+              y: value,
+            })),
+          },
+        ]} color={'#1154FF'} id="volume"/> 
+      </div>
+      <div className="volume-details"></div>
+      
     </>
-  );
+  ); 
 }
