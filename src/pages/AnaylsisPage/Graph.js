b4a3b829a29eb19f65df53e281621d6e8e1ffd54
eugene663 <yujin663@gmail.com>
2023-06-06 22:52:00 +0900

    chore: x축 삭제

--- src/pages/AnaylsisPage/Graph.js
+++ src/pages/AnaylsisPage/Graph.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { ResponsiveLine } from '@nivo/line'
 
-const Graph  = ({ data, color, id}) => {
+const Graph  = ({ data, color, id, unit}) => {
   return (
     <ResponsiveLine
-    colors={color}  // color를 props로 받아서 설정해줍니다.
-    theme={{  // theme에서 x, y축 글씨 색을 바꿔줍니다.
+    colors={color} 
+    theme={{
       textColor: "white",
     }}
       data={data}
@@ -17,21 +17,13 @@ const Graph  = ({ data, color, id}) => {
       curve="catmullRom"
       axisTop={null}
       axisRight={null}
-      axisBottom={{
-        orient: 'bottom',
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0, // tickRotation 값이 바뀝니다.
-        legend: 'time',  // legend 값이 바뀝니다.
-        legendOffset: 36,
-        legendPosition: 'middle'
-    }}
+      axisBottom={null}
       axisLeft={{
         orient: 'left',
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: id,  // legend 값이 바뀝니다.
+        legend: `${id} (${unit})`,  
         legendOffset: -40,
         legendPosition: 'middle'
     }}
@@ -40,7 +32,7 @@ const Graph  = ({ data, color, id}) => {
       pointBorderWidth={2}
       pointBorderColor={{ from: 'serieColor' }}
       pointLabelYOffset={-12}
-      legends={[]} // 그래프 오른쪽의 포인트를 지워줍니다.
+      legends={[]} 
       enableGridX={false}
       enableGridY={false}
       enablePoints={false}
@@ -51,6 +43,5 @@ const Graph  = ({ data, color, id}) => {
 
 export default Graph;
 
-// 그래프 라이브러리에 높이 값을 주지 않으면 그래프가 나타나지 않습니다.
-// 높이값을 꼭 주세요!
+
 
