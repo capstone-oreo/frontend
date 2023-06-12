6512cc1dc7341886bb8d00b2ed78bd0f53d45a66
eugene663 <yujin663@gmail.com>
2023-06-12 14:51:21 +0900

    chore: 반응형 페이지 수정 및 에러 메세지 alert 출력

--- src/pages/LoadingPage/LoadingPage.js
+++ src/pages/LoadingPage/LoadingPage.js
@@ -1,6 +1,17 @@
 import React from 'react'; 
 import "../../css/LoadingPage.css"
 import {Oval} from "react-loader-spinner"; 
+import { useMediaQuery } from 'react-responsive';
+
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 992 })
+  return isDesktop ? children : null
+}
+
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+  return isMobile ? children : null
+}
 
 const clickHome = (e) =>{
   window.location.href ="/";
@@ -11,15 +22,31 @@ const handleClickHistory = (e) =>{
 export default function Loding () { 
   return ( 
     <div>
+    <Mobile>
+      <>
+        <div className="speech-mobile" onClick={clickHome}>Speech</div>  
+        <div className="maru-mobile"onClick={clickHome}>Maru</div>
+        <div className="history-mobile" onClick={handleClickHistory}>history</div> 
+        <div className='loading-mobile'>
+        <div className='loading-text-mobile'>발표를 분석 중입니다. <br/> 잠시만 기다려 주세요.<br/><br/></div>
+    
+        <Oval color='#ffffff' height={70} width={70} />
+      </div>  
+      </>
+    </Mobile>
+    <Desktop>
+    <>
       <div className="speech" onClick={clickHome}>Speech</div>  
       <div className="maru"onClick={clickHome}>Maru</div>
       <div className="history" onClick={handleClickHistory}>history</div>
-    <div className='loading'>
-    <div className='loading-text'>발표를 분석 중입니다. <br/> 잠시만 기다려 주세요.<br/><br/></div>
+      <div className='loading'>
+      <div className='loading-text'>발표를 분석 중입니다. <br/> 잠시만 기다려 주세요.<br/><br/></div>
     
-  <Oval color='#ffffff' height={150} width={150} />
-  </div>
-  </div>
+      <Oval color='#ffffff' height={150} width={150} />
+      </div>
+  </>
+    </Desktop>
+    </div>
   ) ;
 };
 
